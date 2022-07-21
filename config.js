44b93251304b609963ea9560974103db226eaca4
Ahastari <ajassanchez@gmail.com>
2022-07-21 14:59:59 -0500

    cambios

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
         host: "localhost",
         user: "root",
         password: "",
-        database: "clientserver"
+        database: "clientserverA"
     },
     listPerPage: 10
 }
